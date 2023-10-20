36241922e5c859b1ad98f056e3b6ed2279b3b0a2
tiagorahal <rahal.aires@gmail.com>
2023-10-20 11:15:54 -0300

    Add /help command

--- lib/bot.rb
+++ lib/bot.rb
@@ -19,14 +19,17 @@ class Bot
         bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
         
       when  '/hello'
-      greeting = Greeting.new
-      value = greeting.select_random
-      bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
+        greeting = Greeting.new
+        value = greeting.select_random
+        bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
 
       when  '/trivia'
-      trivia = Trivia.new
-      value = trivia.select_random
-      bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
+        trivia = Trivia.new
+        value = trivia.select_random
+        bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
+
+      when '/help'
+        bot.api.send_message(chat_id: message.chat.id, text: "Here are the available commands: \n/start - Start the bot \n/stop - Stop the bot\n/hello - Receive a greeting from Nurgle \n/trivia - Fetch a random fact about Papa Nurgle")
 
       else
         bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /hello, /trivia")

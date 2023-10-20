b5345029f4609483a4b9ec6aaa4aeb29e92bc4e8
tiagorahal <rahal.aires@gmail.com>
2023-10-20 10:15:30 -0300

    Fix nurgle issue

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,5 +1,6 @@
 require 'telegram/bot'
 require_relative 'motivate.rb'
+require_relative 'nurgle.rb'
 
 
 class Bot
@@ -11,16 +12,18 @@ class Bot
       case message.text
       when '/start'
 
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome to Papa Nurgle chat bot created by Tiago. Use  /start to start the bot,  /stop to end the bot, /motivate to get a diffrent motivational quote everytime you request for it.")
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome to Papa Nurgle chat bot created by Tiago. Use  /start to start the bot,  /stop to end the bot, /nurgle to get the blessing.")
 
       when '/stop'
 
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
-      when '/motivate'
-        values = Motivate.new
-        value = values.select_random
-        bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
-      else bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
+        bot.api.send_message(chat_id: message.chat.id, text: "Papa Nurgle bless upon you, #{message.from.first_name}", date: message.date)
+
+      when  '/nurgle'
+      nurgle = Nurgle.new
+      value = nurgle.select_random
+      bot.api.send_message(chat_id: message.chat.id, text: "\"#{value['text']}\" - #{value['author']}", date: message.date)
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
       end
     end
   end
